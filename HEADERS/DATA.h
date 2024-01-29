23b161fd97a789b2c3a4c1766cac009ee4c868de
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-01-29 18:44:23 +0900

    Update DATA.h

--- HEADERS/DATA.h
+++ HEADERS/DATA.h
@@ -2,7 +2,129 @@
 # define DATA_H
 
 # include <iostream>
+# include <vector>
+# include <string>
 
 using namespace std;
+
+// List of user-selectable jobs combining elements of history, imagination, mythology, fantasy, reality, machinery, animals, nature, space, horror, fantasy, the inexplicable, riddles, ghost stories, the future, and fictitious facts.
+vector<string> userSelectableJobs = {
+    "Time Traveling Archaeologist",
+    "Dream Weaver",
+    "Mythical Creature Trainer",
+    "Reality Bender",
+    "Steampunk Engineer",
+    "Cosmic Navigator",
+    "Beast Whisperer",
+    "Nature's Alchemist",
+    "Astronaut Sorcerer",
+    "Fear Architect",
+    "Imagination Sculptor",
+    "Enigma Detective",
+    "Invisible Illusionist",
+    "Riddle Solver",
+    "Ghost Story Archaeologist",
+    "Futuristic Storyteller",
+    "Unicorn Rancher",
+    "Parallel Universe Cartographer",
+    "Nightmare Technician",
+    "Infinite Possibility Explorer"
+};
+
+// List of NPCs with whom the user can interact, blending elements of history, imagination, mythology, fantasy, reality, machinery, animals, nature, space, horror, fantasy, the inexplicable, riddles, ghost stories, the future, and fictitious facts.
+vector<string> interactableNPCs = {
+    "Wandering Time Sage",
+    "Illusionary Bard",
+    "Mythical Tavern Keeper",
+    "Quantum Reality Observer",
+    "Steam-powered Robot Companion",
+    "Celestial Beast Guide",
+    "Whimsical Nature Spirit",
+    "Galactic Oracle",
+    "Horror Enigma Merchant",
+    "Fantasy Artisan",
+    "Mysterious Sphinx",
+    "Invisible Ghost Whisperer",
+    "Riddle-solving Poltergeist",
+    "Dream Eater",
+    "Time-Traveling Phantom",
+    "Future Archaeologist",
+    "AI-powered Unicorn Companion",
+    "Interdimensional Storyteller",
+    "Fear-Inducing Shadow Being",
+    "Living Enigma Puzzle"
+};
+
+// List of friendly entities inspired by history, imagination, mythology, fantasy, reality, machinery, animals, nature, space, horror, fantasy, the inexplicable, riddles, ghost stories, the future, and fictitious facts.
+vector<string> friendlyEntities = {
+    "Ancient Guardian Spirit",
+    "Dreamland Fairy",
+    "Wisdom-infused Griffin",
+    "Reality-defying Robot Companion",
+    "Friendly Time Traveler",
+    "Cosmic Butterfly",
+    "Whimsical Forest Elemental",
+    "Wise Nebula Spirit",
+    "Space-hopping Bunny",
+    "Courageous Horror-fighting Companion",
+    "Imaginary Friend",
+    "Enigmatic Guardian Sphinx",
+    "Invisible Ally",
+    "Riddle-solving Ghost Companion",
+    "Dream Protector",
+    "Future-friendly Extraterrestrial",
+    "Kind-hearted AI Unicorn",
+    "Parallel Universe Ally",
+    "Joyful Nightmare Defender",
+    "Riddle-solving Mythical Being"
+};
+
+// List of neutral entities inspired by history, imagination, mythology, fantasy, reality, machinery, animals, nature, space, horror, fantasy, the inexplicable, riddles, ghost stories, the future, and fictitious facts.
+vector<string> neutralEntities = {
+    "Ancient Relic Guardian",
+    "Illusory Chameleon",
+    "Mystical Traveling Merchant",
+    "Quantum Observer Drone",
+    "Clockwork Owl Companion",
+    "Astral Wanderer",
+    "Elemental Balance Keeper",
+    "Star Gazer",
+    "Interdimensional Nomad Fox",
+    "Enigmatic Library Guardian",
+    "Imagination Collector",
+    "Sphinx's Riddle Guardian",
+    "Invisible Watcher",
+    "Riddle Master Ghost",
+    "Dream Guide",
+    "Time-warping Explorer",
+    "AI Unicorn Observer",
+    "Interdimensional Nomad",
+    "Shadow Realm Watcher",
+    "Mysterious Puzzle Keeper"
+};
+
+// List of hostile entities inspired by history, imagination, mythology, fantasy, reality, machinery, animals, nature, space, horror, fantasy, the inexplicable, riddles, ghost stories, the future, and fictitious facts.
+vector<string> hostileEntities = {
+    "Temporal Anomaly Hunter",
+    "Nightmare Harbinger",
+    "Mythical Beast Hunter",
+    "Reality-warping Trickster",
+    "Steampunk Arachnid Mechanism",
+    "Galactic Shadow Stalker",
+    "Nature's Vengeful Spirit",
+    "Cosmic Chaos Bringer",
+    "Horror-inducing Specter",
+    "Illusion Shatterer",
+    "Nightmarish Puppeteer",
+    "Invisible Phantom Menace",
+    "Riddle-twisting Poltergeist",
+    "Dream Eater Nightmare",
+    "Time-bending Spectral Assassin",
+    "Future Archaeological Saboteur",
+    "Malevolent AI Unicorn",
+    "Interdimensional Menace",
+    "Fear-eating Shadow Beast",
+    "Riddle-spewing Dark Entity"
+};
 
 #endif
