6b155222d4e1ed9312dcd232bcf36e663c10235a
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-01-30 17:24:20 +0900

    Create ITEMS.h

---
+++ SYSTEM/ITEMS.h
@@ -0,0 +1,104 @@
+#ifndef ITEMS_H
+# define ITEMS_H
+
+# include <iostream>
+# include <vector>
+# include <string>
+
+using namespace std;
+
+// Consumables
+vector<string> healingItems = {
+    "Health Potion",
+    "Healing Elixir",
+    "Revitalizing Herb",
+    "Rejuvenation Tonic",
+    "Recovery Salve",
+    "Renewal Potion",
+    "Restorative Elixir",
+    "Vitality Extract",
+    "Healing Crystal",
+    "Revive Syrup"
+};
+
+vector<string> utilityItems = {
+    "Adaptive Camouflage Cloak",
+    "Ethereal Teleportation Stone",
+    "Invisibility Potion",
+    "Dimensional Pocket Watch",
+    "Gravity-defying Boots",
+    "Energy-boosting Gauntlet",
+    "Mind-controlling Amulet",
+    "Illusionary Smoke Bomb",
+    "Temporal Distortion Device",
+    "Lunar Phase Shifter"
+};
+
+vector<string> buffItems = {
+    "Strength-Enhancing Elixir",
+    "Agility-boosting Serum",
+    "Intellect-boosting Potion",
+    "Fortitude Tonic",
+    "Luck Infusion Elixir",
+    "Speed Booster Serum",
+    "Resistance-boosting Draught",
+    "Wisdom Amplification Elixir",
+    "Evasion Elixir",
+    "Focus Enhancement Brew"
+};
+
+// Equipment
+vector<string> weapons = {
+    "Legendary Sword of Justice",
+    "Enchanted Bow of Precision",
+    "Arcane Staff of Destruction",
+    "Mystical Dagger of Shadows",
+    "Thunderstrike Hammer",
+    "Venomous Crossbow",
+    "Firebrand Greatsword",
+    "Frostbite Wand",
+    "Soulbound Katana",
+    "Celestial Scepter"
+};
+
+vector<string> armorSets = {
+    "Dragonscale Armor",
+    "Celestial Robes",
+    "Shadow Veil Cloak",
+    "Titanic Plate Mail",
+    "Stealth Assassin Garb",
+    "Frozen Heart Plate",
+    "Ethereal Silk Robes",
+    "Inferno Battlegear",
+    "Guardian's Aegis",
+    "Nature's Embrace Ensemble"
+};
+
+vector<string> accessories = {
+    "Amulet of Stealth",
+    "Ring of Elemental Resistance",
+    "Bracelet of Time Distortion",
+    "Necklace of Teleportation",
+    "Gloves of Power",
+    "Earrings of Wisdom",
+    "Belt of Strength",
+    "Boots of Speed",
+    "Crown of Arcane Mastery",
+    "Brooch of Luck"
+};
+
+// Artifacts
+vector<string> magicalArtifacts = {
+    "Crystal of Time Manipulation",
+    "Orb of Elemental Mastery",
+    "Aegis of Divine Protection",
+    "Amulet of Ancient Wisdom",
+    "Phoenix Feather Talisman",
+    "Chalice of Eternal Youth",
+    "Starlight Prism Pendant",
+    "Scepter of Arcane Power",
+    "Oculus of Astral Vision",
+    "Eternal Flame Relic"
+};
+
+#endif
