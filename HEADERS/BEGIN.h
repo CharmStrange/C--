41341470d017f40be73a5d50576a72009ff5f4f2
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-05 18:33:02 +0900

    Update BEGIN.h

--- HEADERS/BEGIN.h
+++ HEADERS/BEGIN.h
@@ -10,47 +10,61 @@
 using namespace std;
 
 // main process
-void START_SCREEN();
+unsigned char start_value;
+
+void START_SCREEN(); // Forward Declaration
 void RUN_BEGIN() {
-    START_SCREEN();
+    cout << "Welcome, this is 'Game Title'. Try various functions of 'Game Title' and take what you want.\n" << endl;
+    do { START_SCREEN(); } while (start_value == 1);
 }
 
 // process functions
-void user_start(char alphabet);
+void user_start(char alphabet); // Forward Declaration
 void START_SCREEN() {
     char alphabet;
 
-    cout << "Welcome, this is 'Game Title'. Try various functions of 'Game Title' and take what you want.\n" << endl;
     cout << "[N] : New Game" << endl;
     cout << "[K] : Keep Playing" << endl;
     cout << "[C] : Credit" << endl;
+    cout << "[X] : Exit" << endl;
 
     cin >> alphabet;
     user_start(alphabet);
 }
 
 void user_start(char alphabet) {
+    start_value = 1; // Reset start_value to 1 for the next iteration
     switch (alphabet) {
         case 'N':
         //case 'n':
 
             cout << "Starting a New Game..." << endl;
             // Add the logic for starting a new game
+            start_value = 0;
             break;
+            
         case 'K':
         //case 'k':
 
             cout << "Keep Playing..." << endl;
             // Add the logic for continuing the game
+            start_value = 0;
             break;
-        case 'O':
-        //case 'o':
+            
+        case 'C':
+        //case 'c':
 
             cout << "Credit..." << endl;
             // Add the logic for displaying and handling options
+            start_value = 0;
             break;
+            
+        case 'X':
+        //case 'x':
+            exit(0);
+            
         default:
-            cout << "Invalid choice. Please choose [N], [K], or [O]." << endl;
+            cout << "Invalid choice. Please choose [N], [K], or [O].\n" << endl;
     }
 }
 
