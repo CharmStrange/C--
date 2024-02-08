da317a9bcdf65dc9551d01f591fb1a17dc819db9
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-08 21:59:17 +0900

    from VSCode, 20240208

--- HEADERS/BEGIN.h
+++ HEADERS/BEGIN.h
@@ -10,52 +10,87 @@
 using namespace std;
 
 // main process
-void START_SCREEN();
+unsigned char start_value;
+
+void START_SCREEN(); // Forward Declaration
+
 void RUN_BEGIN() {
-    START_SCREEN();
+    cout << "Welcome, this is 'Game Title'. Try various functions of 'Game Title' and take what you want.\n" << endl;
+    do { START_SCREEN(); } while (start_value == 1);
 }
 
 // process functions
-void user_start(char alphabet);
+void user_start(char alphabet); // Forward Declaration
+void NEW_GAME(); // Forward Declaration
+void KEPP_PLAYING(); // Forward Declaration
+void CREDIT_SCREEN(); // Forward Declaration
+
 void START_SCREEN() {
     char alphabet;
 
-    cout << "Welcome, this is 'Game Title'. Try various functions of 'Game Title' and take what you want.\n" << endl;
     cout << "[N] : New Game" << endl;
     cout << "[K] : Keep Playing" << endl;
     cout << "[C] : Credit" << endl;
+    cout << "[X] : Exit" << endl;
 
     cin >> alphabet;
     user_start(alphabet);
 }
 
 void user_start(char alphabet) {
+    start_value = 1; // Reset start_value to 1 for the next iteration
     switch (alphabet) {
         case 'N':
         //case 'n':
-
             cout << "Starting a New Game..." << endl;
-            // Add the logic for starting a new game
+            start_value = 0;
+            NEW_GAME();
             break;
+            
         case 'K':
         //case 'k':
-
-            cout << "Keep Playing..." << endl;
+            cout << "Keep Playing...\n" << endl;
             // Add the logic for continuing the game
+            start_value = 0;
             break;
-        case 'O':
-        //case 'o':
-
-            cout << "Credit..." << endl;
-            // Add the logic for displaying and handling options
+            
+        case 'C':
+        //case 'c':
+            cout << "Credit...\n" << endl;
+            CREDIT_SCREEN();
             break;
+            
+        case 'X':
+        //case 'x':
+            exit(0);
+            
         default:
-            cout << "Invalid choice. Please choose [N], [K], or [O]." << endl;
+            cout << "Invalid choice. Please choose [N], [K], or [O].\n" << endl;
     }
 }
+///  
+///
+///
+void NEW_GAME() {
+    string new_save_name;
+    cout << "Type player name :" << endl;
+    cin >> new_save_name;
+    PLAYER_SAVE NEW_ONE(new_save_name);
+    NEW_ONE.display_saves();
+}
 
+void KEEP_PLAYING() {
+    
+}
+/// 
+///
+///
 void CREDIT_SCREEN() {
-
+    cout << "Developer : CharmStrange / Intelligence Utility" << endl;
+    cout << "Developing Language : C++" << endl;
+    cout << "Developing Period : 2024-01-24 ~ " << endl;
+    cout << "GitHub : https://github.com/CharmStrange" << endl;
+    cout << "(return to start page...)\n" << endl;
 }
 
 #endif
