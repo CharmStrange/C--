aaa88d8a1383969e2bd6c43f2ec4493760fabdbe
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-02-24 23:50:01 +0900

    from VSCode

--- HEADERS/BEGIN.h
+++ HEADERS/BEGIN.h
@@ -79,6 +79,7 @@ void user_start(char alphabet) {
 void START_GAME() {
     // use GAME_ALGORITHM.h
     cout << "\nThe game started." << endl;
+    CHOOOSE_JOB();
 }
 
 // N
--- HEADERS/GAME_ALGORITHM.h
+++ HEADERS/GAME_ALGORITHM.h
@@ -3,9 +3,35 @@
 
 # include <iostream>
 # include <string>
+# include "JOBS.h"
 
 using namespace std;
 
+void CHOOOSE_JOB() {
+    unsigned short A;
+    cout << "There are 3 regions, 'Industrial Zone', 'Desolate Area', 'Peaceful Village'." << endl;
+    cout << "Choose what you want - " << endl;
+    cout << "[1] : 'Industrial Zone' \n[2] : 'Desolate Area' \n[3] : 'Peaceful Village'\n :";
+    cin >> A;
 
+    if (A == 1) {
+        for (const string& index : industrialZoneJobs) {
+            cout << "\n" << index << endl;
+        }
+    }
+
+    else if (A == 2) {
+        for (const string& index : desolateAreaJobs) {
+            cout << "\n" << index << endl;
+        }
+    }
+
+    else {
+        for (const string& index : peacefulVillageJobs) {
+            cout << "\n" << index << endl;
+        }
+    }
+
+}
 
 #endif
