b7f715f501c92d9984cdc7af415c4d6fe828ca54
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-06-04 18:42:09 +0900

    Update DataFlowExample.cpp

--- Source Coder/DataFlowExample.cpp
+++ Source Coder/DataFlowExample.cpp
@@ -5,18 +5,20 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+
 // 데이터 구조체 정의
 struct Data {
-    std::string header_info;
-    std::string actual_data;
-    std::string dummy_data; // 필요에 따라 사용될 수 있는 더미 데이터
+    string header_info;
+    string actual_data;
+    string dummy_data; // 필요에 따라 사용될 수 있는 더미 데이터
 };
 
 class DataGenerator {
 public:
     DataGenerator() {
         // 시드 초기화
-        std::srand(std::time(nullptr));
+        srand(time(nullptr));
     }
 
     Data generateData() {
@@ -25,7 +27,7 @@ public:
         newData.actual_data = generateActualData();
         
         // 랜덤으로 더미 데이터를 생성할지 결정
-        if (std::rand() % 2) {
+        if (rand() % 2) {
             newData.dummy_data = generateDummyData();
         } else {
             newData.dummy_data = "";
@@ -35,14 +37,14 @@ public:
     }
 
 private:
-    std::string generateHeaderInfo() {
+    string generateHeaderInfo() {
         // 간단한 예로 헤더 정보를 생성
-        return "Header_" + std::to_string(std::rand() % 1000);
+        return "Header_" + to_string(rand() % 1000);
     }
 
-    std::string generateActualData() {
+    string generateActualData() {
         // 간단한 예로 실제 데이터를 생성
-        return "Data_" + std::to_string(std::rand() % 10000);
+        return "Data_" + to_string(rand() % 10000);
     }
 
     std::string generateDummyData() {
@@ -54,12 +56,12 @@ private:
 class DataClassifier {
 public:
     // 파이프라인을 설정
-    void addPipeline(const std::string& header, const std::string& pipeline) {
+    void addPipeline(const string& header, const string& pipeline) {
         pipelines[header] = pipeline;
     }
 
     // 데이터 분류
-    std::string classify(const Data& data) {
+    string classify(const Data& data) {
         if (pipelines.find(data.header_info) != pipelines.end()) {
             return pipelines[data.header_info];
         } else {
@@ -68,30 +70,30 @@ public:
     }
 
 private:
-    std::unordered_map<std::string, std::string> pipelines;
+    unordered_map<string, string> pipelines;
 };
 
 class DataWarehouse {
 public:
-    void storeData(const std::string& pipeline, const Data& data) {
+    void storeData(const string& pipeline, const Data& data) {
         warehouse[pipeline].push_back(data);
     }
 
     void displayWarehouse() const {
         for (const auto& entry : warehouse) {
-            std::cout << "Pipeline: " << entry.first << std::endl;
+            cout << "Pipeline: " << entry.first << endl;
             for (const auto& data : entry.second) {
-                std::cout << "  Header: " << data.header_info << ", Data: " << data.actual_data;
+                cout << "  Header: " << data.header_info << ", Data: " << data.actual_data;
                 if (!data.dummy_data.empty()) {
-                    std::cout << ", Dummy: " << data.dummy_data;
+                    cout << ", Dummy: " << data.dummy_data;
                 }
-                std::cout << std::endl;
+                cout << endl;
             }
         }
     }
 
 private:
-    std::unordered_map<std::string, std::vector<Data>> warehouse;
+    unordered_map<string, vector<Data>> warehouse;
 };
 
 int main() {
@@ -107,7 +109,7 @@ int main() {
     // 예제: 10개의 데이터를 생성, 분류 및 저장
     for (int i = 0; i < 10; ++i) {
         Data data = generator.generateData();
-        std::string pipeline = classifier.classify(data);
+        string pipeline = classifier.classify(data);
         warehouse.storeData(pipeline, data);
     }
 
