c4c0dde3aa8fde630bfaaa4ab69afcc7b3c7b03e
LKH <105769152+CharmStrange@users.noreply.github.com>
2025-08-16 08:30:40 +0900

    Update player.h

--- Savior/player.h
+++ Savior/player.h
@@ -33,37 +33,6 @@ public:
         cout << "---------------------\n\n";
 	}
 	
-    // 플레이어의 액션을 나타내는 메서드
-    void pray(int amount);     // 신앙심에 따라 효과가 달라짐
-    void confront();           // 타락한 성직자와 대면
-    void useAbility(const string& abilityName);
-	
-    void loseFaith(int amount) {
-        faith -= amount;
-        if (faith < FaithThreshold) faith = 0;
-        /// cout << "당신의 신앙이 흔들립니다. (" << amount << " 감소)\n";
-    }
-
-    void gainRage(int amount) {
-        rage += amount;
-        if (rage > rageThreshold) rage = 100;
-        // cout << "분노가 솟아오릅니다. (" << amount << " 증가)\n";
-    }
-
-    void acquirePower(int amount) {
-        power += amount;
-        // cout << "잠재된 힘이 커집니다. (" << amount << " 증가)\n";
-    }
-
-    void useRageAbility() {
-        if (rage >= rageThreshold) {
-            // cout << "분노를 소모하여 강력한 힘을 사용합니다!\n";
-            rage -= 20;
-            // ... 능력 발동 로직
-        } else {
-            /// std::cout << "분노가 충분하지 않습니다.\n";
-        }
-    }
 };
 
 #endif
