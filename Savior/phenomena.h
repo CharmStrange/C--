7684d04d7839c22ebc243f529fd220a4b69150fe
LKH <105769152+CharmStrange@users.noreply.github.com>
2025-08-20 19:20:42 +0900

    Update phenomena.h

--- Savior/phenomena.h
+++ Savior/phenomena.h
@@ -7,4 +7,16 @@
 
 #include <vector>
 
+using namespace std;
+
+class Phenomenon {
+private:
+	string name;
+	string type;
+	unsigned short number;
+	
+public:
+	Phenomenon(const string& name, const string& type, unsigned short number);
+};
+
 #endif
