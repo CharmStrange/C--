a32c514df54146704a90e16d98aad0552241de96
LKH <105769152+CharmStrange@users.noreply.github.com>
2025-08-30 13:15:14 +0900

    Update phenomena.h

--- Savior/phenomena.h
+++ Savior/phenomena.h
@@ -14,9 +14,13 @@ private:
 	string name;
 	string type;
 	unsigned short number;
+	unsigned short energy;
+	short faith;
+	unsigned short rage;
+	unsigned short power;
 	
 public:
-	Phenomenon(const string& name, const string& type, unsigned short number);
+	Phenomenon(const string& name, const string& type, unsigned short number, unsigned short energy, short faith, unsigned short rage, unsigned short power) : name(name), type(type), number(number), energy(energy), faith(faith), rage(rage), power(power) {}
 };
 
 #endif
